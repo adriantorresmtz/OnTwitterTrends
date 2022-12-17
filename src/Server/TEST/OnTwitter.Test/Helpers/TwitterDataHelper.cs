fe2934a77a3f412aa0da3b63484e643f80d3d6c6
adriantorresmtz <adriantorresmtz@hotmail.com>
2022-12-17 14:34:13 -0600

    Refactor Unit Test

---
+++ src/Server/TEST/OnTwitter.Test/Helpers/TwitterDataHelper.cs
@@ -0,0 +1,32 @@
+﻿using OnTwitter.Domain.Entities;
+
+namespace OnTwitter.Test.Helpers;
+
+public static class TwitterDataHelper
+{
+    public static List<Twitter> GetTwitters_Fake() 
+    {
+        var twitters = new List<Twitter>() {
+            new Twitter(){ Id = 1, TwitterId= "124568"},
+            new Twitter(){ Id = 2, TwitterId= "958475"},
+            new Twitter(){ Id = 3, TwitterId= "487512"},
+            new Twitter(){ Id = 4, TwitterId= "658741"},
+            new Twitter(){ Id = 5, TwitterId= "772230"},
+        };
+
+        return twitters;
+    }
+
+    public static List<TwitterHashTag> GetTwitterHashTags_Fake()
+    {
+        var twitterHashTags = new List<TwitterHashTag>() {
+            new TwitterHashTag(){ Id = 1, TwitterId= "124568", HashTag="Developer"},
+            new TwitterHashTag(){ Id = 2, TwitterId= "958475", HashTag="Developer"},
+            new TwitterHashTag(){ Id = 3, TwitterId= "487512", HashTag="Developer"},
+            new TwitterHashTag(){ Id = 4, TwitterId= "658741", HashTag="Tester"},
+            new TwitterHashTag(){ Id = 5, TwitterId= "772230", HashTag="Tester"},
+        };
+
+        return twitterHashTags;
+    }
+}
---
+++ src/Server/TEST/OnTwitter.Test/TwitterServiceTests.cs
@@ -0,0 +1,120 @@
+﻿
+using Moq;
+using OnTwitter.Application.Common.Interfaces;
+using OnTwitter.Infrastructure.Services;
+using OnTwitter.Test.Helpers;
+using AutoMapper;
+using OnTwitter.Domain.Entities;
+using OnTwitter.Application.Models;
+using NuGet.Frameworks;
+using FluentAssertions;
+
+namespace OnTwitter.Test;
+
+public class TwitterServiceTests
+{
+    #region Declarations
+
+    private readonly TwitterService _sut;
+    private readonly IMapper _mapper;
+    private readonly Mock<IUnitOfWork> _uowMock = new Mock<IUnitOfWork>();
+
+    #endregion
+
+    #region Constructor
+
+    public TwitterServiceTests()
+    {
+        #region MapperConfiguration
+        var mapperConfig = new MapperConfiguration(c =>
+        {
+            c.AddProfile<AutoMapperProfile>();
+        });
+        _mapper = mapperConfig.CreateMapper();
+        #endregion
+        _sut = new TwitterService(_uowMock.Object, _mapper);
+    }
+
+    #endregion
+
+
+    [Fact]
+    public async Task InsertTwitter_ShouldReturnTrue()
+    {
+        // Arrange
+        var twitterDto = new TwitterDto() { TwitterId = "458789", TwitterAuthor = "23563214" };
+        _uowMock.Setup(x => x.Twitters.Add(It.IsAny<Twitter>())).ReturnsAsync(true);
+
+        //Act
+        var result = await _sut.InsertTwitter(twitterDto);
+
+        // Assert
+        Assert.True(result);
+
+    }
+
+    [Fact]
+    public async Task InsertTwitterHastTag_ShouldReturnTrue()
+    {
+        // Arrange
+        var twitterHashTagDto = new TwitterHashTagDto() { TwitterId = "458789", HashTag = "Developer"};
+        _uowMock.Setup(x => x.TwitterHashTags.Add(It.IsAny<TwitterHashTag>())).ReturnsAsync(true);
+
+        //Act
+        var result = await _sut.InsertTwitterHastTag(twitterHashTagDto);
+
+        // Assert
+        Assert.True(result);
+
+    }
+
+
+    [Fact]
+    public async Task GetTwitters_ShouldReturnTwitters() 
+    {
+        // Arrange
+        var twitters = TwitterDataHelper.GetTwitters_Fake();
+        _uowMock.Setup(x => x.Twitters.GetAll()).ReturnsAsync(twitters);
+
+        // Act
+        var results = await _sut.GetTwitters();
+
+        // Assert
+        Assert.Equal(twitters.Count(), results.Count());
+    }
+
+
+    [Fact]
+    public async Task GetTwittersTotal_ShouldReturnTwittersTotal() 
+    {
+        // Arrange
+        var totalTwitters = 5;
+        var twitters = TwitterDataHelper.GetTwitters_Fake();
+        _uowMock.Setup(x => x.Twitters.GetAll()).ReturnsAsync(twitters);
+
+        // Act
+        var result = await _sut.GetTwittersTotal();
+
+        // Assert
+        Assert.Equal(totalTwitters, result);
+    }
+
+    [Fact]
+    public async Task GetTwitters_ShouldReturnTwitterHasTags()
+    {
+        // Arrange
+        var twitterHashTags = TwitterDataHelper.GetTwitterHashTags_Fake();
+        _uowMock.Setup(x => x.TwitterHashTags.GetAll()).ReturnsAsync(twitterHashTags);
+
+        // Act
+        var results = await _sut.GetTwitterHashTagsTop(10);
+
+        // Assert
+        Assert.Equal(5, results.TotalTwitterHashTagsTotal);
+        Assert.Equal(3, results.TwitterHashTagTops[0].HashTagTotal);
+        Assert.Equal(2, results.TwitterHashTagTops[1].HashTagTotal);
+
+    }
+
+
+}
